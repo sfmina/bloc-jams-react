2c2b9a13374845d32e0ac2ecdb419a40c662d1ef
sfmina <minafranke@gmail.com>
2018-09-20 09:11:23 -0700

    Add next button function

--- src/components/Album.js
+++ src/components/Album.js
@@ -77,6 +77,14 @@ import PlayerBar from './PlayerBar';
       this.play();
    }
 
+   handleNextClick() {
+    const currentIndex =  this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+   }
+
 
    render() {
      return (
@@ -116,6 +124,7 @@ import PlayerBar from './PlayerBar';
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
           />
          </section>
      );
