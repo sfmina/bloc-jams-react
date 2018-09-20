9f1d1ed79848007a9e442372a99c5f2ea2e57e76
sfmina <minafranke@gmail.com>
2018-09-20 08:57:18 -0700

    Playbar function

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
  class Album extends Component {
   constructor(props) {
@@ -68,6 +69,14 @@ import albumData from './../data/albums';
      return 'show';
    }
 
+   handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+   }
+
 
    render() {
      return (
@@ -102,6 +111,12 @@ import albumData from './../data/albums';
            
            </tbody>
          </table>
+         <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+          />
          </section>
      );
    }
